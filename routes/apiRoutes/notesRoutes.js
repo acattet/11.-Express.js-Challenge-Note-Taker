7b429cc0141a8e7a28f3746114a21ebcf2c4a594
Anthony <anthony.cattet@gmail.com>
2022-07-05 22:25:42 -0400

    fixed issues

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { createNewNote, validateNote } = require("../../lib/notes.js");
 const { notes } = require("../../db/db.json");
+const { createNewNote, validateNote } = require("../../lib/notes.js");
+
 
 router.get("/notes", (req, res) => {
     res.json(notes);
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -5,12 +5,14 @@ router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/index.html"));
-});
+
 
 module.exports = router;
